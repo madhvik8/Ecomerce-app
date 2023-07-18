81986128ac530225c40f709b91ab54ad73ed9dde
Madhvi Kunwar Rana <madhvikrana@gmail.com>
2023-07-18 18:13:26 +0530

    Ecommerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Index";
+import Home from "./modulus/Home";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import Product from "./modulus/Product";
+import Products from "./modulus/Products";
+import CategotyProducts from "./modulus/CategoryProducts";
+import Cart from "./modulus/Cart/Index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<Product/>} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/categories/:name" element={<CategotyProducts/>} />
+        <Route path="/cart" element={< Cart/>} />
+        <Route path="*" element={<div>404</div>} />
+
+      </Routes>
+      <Footer />
     </div>
   );
 }
