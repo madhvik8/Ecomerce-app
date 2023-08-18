08123b19f9d96ba452ff66f1ffcaca3a6b6e878f
Madhvi Kunwar Rana <madhvikrana@gmail.com>
2023-08-18 15:34:55 +0530

    commit3

--- src/modulus/Product/index.js
+++ src/modulus/Product/index.js
@@ -17,7 +17,7 @@ const Product = () => {
   }, []);
   
   if(!Object.keys(product).length > 0 ) return <div>Loading.....</div>
-  !Object.keys(product).length > 0 && <div>Product Not Found</div>;
+  !Object.keys(product).length > 0 && <div>Product Nott Found</div>;
   return (
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
